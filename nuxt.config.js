2858642551dd828741afebc36639e5d5a6be093d
Hoceine <hoceineelidrissi@gmail.com>
2023-10-12 01:41:34 +0100

    testing

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,22 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-   buildModules: [
+  buildModules: [
     'nuxt-vite'
   ],
+  vite: {
+    build: true,
+  },
   devtools: { enabled: false },
 
   app: {
     head: {
-       title:"Hoceine EL IDRISSI's Portfolio",
+      title: "Hoceine EL IDRISSI's Portfolio",
       link: [
         {
           rel: 'icon',
           type: 'image/x-icon',
-          href: '/logo.svg' 
+          href: '/logo.svg'
         }
       ]
-     } 
-   },
+    }
+  },
 
   modules: [
     '@nuxtjs/tailwindcss',
