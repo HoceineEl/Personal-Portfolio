d16bc59fb816db2713ae5e97e906400261b35a61
Hoceine <hoceineelidrissi@gmail.com>
2023-10-14 23:21:43 +0100

    solving some design problem after deployment

--- components/Sections/Tech.vue
+++ components/Sections/Tech.vue
@@ -6,7 +6,7 @@
       :key="tech.name"
       class="flex justify-center items-center w-36 h-36 relative group"
     >
-      <!-- <LazyCanvasObject :object="tech.icon" :scale="1" :speed="10" /> -->
+      <LazyCanvasObject :object="tech.icon" :scale="1" :speed="10" />
       <span
         class="absolute text-[6px] group-hover:text-lg top-3 group-hover:-top-3 text-secondary opacity-0 group-hover:opacity-100 transition duration-1000 bg-tertiary px-3 py-2 rounded-lg"
         >{{ tech.name }}</span
