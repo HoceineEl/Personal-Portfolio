7b216bab4b605f10887ce6c1996e32273f87037a
Hoceine <hoceineelidrissi@gmail.com>
2023-10-29 16:08:34 +0100

    adding the footer

--- components/Sections/footer.vue
+++ components/Sections/footer.vue
@@ -1,12 +1,12 @@
 <template>
-  <footer class="w-full flex">
+  <footer class="w-full flex justify-around items-center py-10 mt-10">
     <p>Hoceine El idrissi &copy 2023</p>
     <div class="flex gap-10">
-      <a href="https://github.com/hoceineel"
-        ><img :src="github" alt="hoceine el idrissi github account" width="50"
+      <a href="https://github.com/hoceineel" target="_blank"
+        ><img :src="github" alt="hoceine el idrissi github account" width="35"
       /></a>
-      <a href="https://linkedin.com/in/elidrissihoceine"
-        ><img :src="linkedin" alt="hoceine el idrissi linkedin account" width="50"
+      <a href="https://linkedin.com/in/elidrissihoceine" target="_blank"
+        ><img :src="linkedin" alt="hoceine el idrissi linkedin account" width="35"
       /></a>
     </div>
   </footer>
