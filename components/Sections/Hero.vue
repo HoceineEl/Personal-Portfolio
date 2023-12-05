bcd65bd50baab7359ea11d4fbd638ec278fbc545
Hoceine <hoceineelidrissi@gmail.com>
2023-12-05 17:01:44 +0100

    update : change the image in og and tw cards

--- components/Sections/Hero.vue
+++ components/Sections/Hero.vue
@@ -30,8 +30,7 @@ import { portrait } from "~/assets/constants";
             Passionate coder crafting digital solutions with creativity and precision.
             Explore more!
           </p>
-
-          <a href="mailto:hoceineelidrissi@gmail.com">
+          <a href="#contact">
             <div
               class="px-6 py-3 text-main-violet font-bold border-2 rounded-xl w-fit group relative hover:text-white-100 overflow-hidden"
             >
