991ade35e7affdd84e69bd3899418560591a21ed
Hoceine <hoceineelidrissi@gmail.com>
2024-02-01 15:32:16 +0100

    update the youtube ad skipper description

--- components/Chunk/ViewSocial.vue
+++ components/Chunk/ViewSocial.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="md:fixed flex md:flex-col gap-2 md:top-1/2 md:right-5">
+  <div class="fixed flex flex-col gap-2 top-1/2 right-5">
     <a v-for="social in socials" :href="social.url" target="_blank">
-      <NuxtImg :src="social.icon" width="35" format="webp" />
+      <NuxtImg :src="social.icon" class="w-9 sm:w-10" format="webp" />
     </a>
   </div>
 </template>
